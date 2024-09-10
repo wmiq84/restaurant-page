e0ca33b9be0ba61dbceb9684434bafffd4c9aaa9
wmiq84 <michaelwang3489@gmail.com>
2024-09-10 12:20:53 -0700

    Add template.html skeleton structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // index.js
-import odinImage from "./odin.png";
+// import odinImage from "./odin.png";
    
-const image = document.createElement("img");
-image.src = odinImage;
+// const image = document.createElement("img");
+// image.src = odinImage;
    
-document.body.appendChild(image);
+// document.body.appendChild(image);
 
 import "./styles.css"
 import { greeting } from "./greeting.js";
