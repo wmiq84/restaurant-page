c67b9fce85c1b91b634ce40b6fd765919496e484
wmiq84 <michaelwang3489@gmail.com>
2024-09-09 19:54:16 -0700

    Initialize files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// index.js
+import odinImage from "./odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+
+import "./styles.css"
+import { greeting } from "./greeting.js";
+
+console.log(greeting);
